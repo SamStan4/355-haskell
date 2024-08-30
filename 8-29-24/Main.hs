c60b8a4ad5cc794912b6be5414986a159d66df6d
samStan4 <sam.charles.stanley@gmail.com>
2024-08-29 19:47:34 -0700

    added class code

---
+++ 8-29-24/Main.hs
@@ -0,0 +1,5 @@
+
+import MathUtil
+
+main = do
+
---
+++ 8-29-24/MathUtil.hs
@@ -0,0 +1,22 @@
+module MathUtil where
+
+add x y = x + y
+
+sub x y = x - y
+
+mul x y = x * y
+
+divi x y = x `div` y
+
+-- this is a function that calculates the length of an array
+len1 [] = 0
+len1 (x : xs) = 1 + len1 xs
+
+-- this is another funciton that calculates the length of an array
+-- it makes use of the ternary operator here in haskell
+len2 xs = if xs == [] then 0 else 1 + len2 (tail xs)
+
+-- this is another cool way of finding the length of an array
+len3 xs
+    | xs == [] = 0
+    | otherwise = 1 + len3 (tail xs)
