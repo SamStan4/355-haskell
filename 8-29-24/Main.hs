f253109bfcbde695cb7e04e2929750514524b4ba
samStan4 <sam.charles.stanley@gmail.com>
2024-09-01 23:10:22 -0700

    shit

--- 8-29-24/Main.hs
+++ 8-29-24/Main.hs
@@ -2,4 +2,11 @@
 import MathUtil
 
 main = do
-
+    putStrLn "Enter your first number: "
+    numberOneString <- getLine
+    putStrLn "Enter your second number: "
+    numberTwoString <- getLine
+    let numberOne = read numberOneString :: Int
+    let numberTwo = read numberTwoString :: Int
+    let numberSum = numberOne + numberTwo
+    putStrLn ("Here is the sum of your two numbers: " ++ show numberSum)
